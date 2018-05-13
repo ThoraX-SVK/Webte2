365c88ae348c129d4c20b55f349c7f0382008e26
ThoraX-SVK <krason.tomas@gmail.com>
2018-05-13 18:38:30 +0200

    Check if user is in DB
    -adding function, that calls DB and either returns userID or null
    if user is not present

--- project/database/userUtils.php
+++ project/database/userUtils.php
@@ -4,16 +4,47 @@
  *  Every call to database that somehow handles users goes here
  */
 
+include_once '../database/createConnection.php';
 
 function getUserIdFromEmail__FAKE($email) {
     return 1;
 }
 
+/**
+ * For testing purposes, substitute this for getUserIdFromEmail() too
+ * see behaviour when user with given email is not in DB
+ *
+ * @param $email
+ * @return null
+ */
+function getUserIdFromEmail_USER_NON_EXISTENT__FAKE($email) {
+    return null;
+}
+
+/**
+ * Returns userID for given email on NULL if email is not found
+ *
+ * @param $email
+ * @return int|null
+ */
 function getUserIdFromEmail($email) {
 
     //TODO: Call DB, return email userID if there is, null if not
 
-    return null;
+    $conn = createConnectionFromConfigFileCredentials();
+    $stmn = $conn->prepare("SELECT User.id FROM w2final.User WHERE email = ?");
+    $stmn->bind_param("s",$email);
+    $stmn->execute();
+
+    $result = $stmn->get_result();
+
+    if(mysqli_num_rows($result) === 0) {
+        //Email not in DB
+        return null;
+    }
+
+    $resultRow = $result->fetch_assoc();
+    return $resultRow['id'];
 }
 
 function isEmailAlreadyInDatabase_TRUE__FAKE($email) {
