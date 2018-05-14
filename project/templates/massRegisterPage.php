282569a6e300c961cb872f20b62573d5b0ac2049
lib <lib>
2018-05-14 23:27:11 +0200

    -added visuals to "massRegisterPage.php" -created a base visuals for whole site -page can be accessed only if user is admin, else redirect to "login.php" -created horizontal menu bar -on the menu bar are listed only proper options (user cant see admins options)

--- project/templates/massRegisterPage.php
+++ project/templates/massRegisterPage.php
@@ -1,53 +1,60 @@
+<?php
+include_once "../utils/sessionUtils.php";
+
+if (!isUserAdmin_YES__FAKE()) {
+    header("Location: login.php");
+    return;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" type="text/css" href="../static/style.css">
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Mass Registration</title>
 </head>
+
 <body>
+<!--TODO: add new function to template-utils that prints whole menu bar -->
+<div class="topnav">
+    <a href="about.php">About</a>
+    <a href="login.php">Login</a>
+    <a href="homePage.php">Home</a>
+    <a href="routesPage.php">Routes</a>
 
-<?php
-    // ERROR SHOWING
-    include_once "../constants/registerConstants.php";
+        <?php
+        //TODO: only echo these options if user is admin
+        if (isUserAdmin_YES__FAKE()) {
+            echo '<div class="topnav-right">';
+            echo '<a class="active" href="massRegisterPage.php">Registrations</a>';
+            echo '</div>';
+        }
+        ?>
 
-    $errorMessage = getErrorMessage();
-    if ($errorMessage != null) {
-        echo "<div class='register-status-message'>";
-        echo $errorMessage;
-        echo "</div>";
-    }
-?>
+</div>
 
 
-<form action="../controller/massRegistrationController.php" method="post" enctype="multipart/form-data">
+<header>
+    <h1>Admin :: Mass Registration</h1>
+</header>
+
+<div class="content">
+
+<form action="../controller/massRegistrationController.php" method="post" enctype="multipart/form-data" class="middle-relative">
 
     <!--  keep  -->
     <input type="file" name="file" id="file"/>
-
+    <br>
+    <br>
     <!--  keep  -->
     <input type="submit" name="submit"/>
 
 </form>
 
+</div>
 
-<?php
 
-function getErrorMessage()
-{
-    if (isset($_GET["status"])) {
-
-        $status = $_GET["status"];
-
-        switch ($status) {
-            case ERROR_USER_NOT_ADMIN:
-                return "Only administrators can mass register users";
-        }
-    }
-
-    return null;
-}
-
-?>
 
 </body>
 </html>
