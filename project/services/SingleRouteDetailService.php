17eec841919b1743742d77bd337de9d144ca9adf
ThoraX-SVK <krason.tomas@gmail.com>
2018-05-16 22:21:18 +0200

    Single route detail basic template
    -adding very basic template to single route detail

---
+++ project/services/SingleRouteDetailService.php
@@ -0,0 +1,15 @@
+<?php
+
+function printLastRunsTable($routeID) {
+
+    //call get_N_lastRuns to get data
+
+}
+
+function printFullDescriptionTable($routeID) {
+
+    //call getRouteFullDescription to get data
+
+}
+
+
---
+++ project/templates/singleRouteDetail.php
@@ -0,0 +1,14 @@
+//TODO: This should display single routePage.php
+
+
+
+//TODO: Get data from _GET (routeID) in PHP
+//TODO: Check if user has visibility on that route
+//TODO: If not throw error and move him back to AllRoues
+
+
+//TODO: Call to singleRouteDetail service for all data to print
+    Maps            ???
+    Info            printLastRunsTable(routeID)
+    Logs            printFullDescriptionTable(routeID)
+    ProgressBar     createProgressBar(routeID)
