0142b926d641839a652d2cc766767835d1ef7ee3
timbez <timbez1996@gmail.com>
2018-05-16 20:39:58 +0200

    news page update (pagination) + newsUtils.php in /database/ - added pagination to news page - added getAllNews() method to get news from DB

---
+++ project/services/printNewsService.php
@@ -0,0 +1,55 @@
+<?php
+
+include_once "../database/newsUtils.php";
+
+define("NEWS_PER_PAGE", 5);
+
+function printNewsByPage($page, $newsPerPage = null) {
+
+    if ($newsPerPage == null) {
+        $newsPerPage = NEWS_PER_PAGE;
+    }
+
+    $allNews = getAllNews__FAKE();
+    $newsForGivenPage = getNewsForGivenPage($allNews, $page, $newsPerPage);
+    $formattedNews = array();
+
+    foreach ($newsForGivenPage as $news) {
+        array_push($formattedNews, constructNews($news));
+    }
+
+    return $formattedNews;
+
+}
+
+function getNewsForGivenPage($allNews, $page, $newsPerPage) {
+    $startingIndex = $newsPerPage * ($page - 1);
+
+    $newsToShow = array_slice($allNews, $startingIndex, $newsPerPage);
+
+    return $newsToShow;
+}
+
+function constructNews($news) {
+    $string = "";
+
+    $string .= '<h2 class="newsHeader">';
+    $string .= $news["header"];
+    $string .= '</h2>';
+
+    $string .= '<hr>';
+
+    $string .= '<p class="newsText">';
+    $string .= $news["content"];
+    $string .= '</p>';
+
+    $string .= '<p class="newsDate">';
+    $string .= $news["added"];
+    $string .= '</p>';
+
+    $string .= "<br/><br/>\n";
+
+    return $string;
+
+}
+
--- project/templates/newsPage.php
+++ project/templates/newsPage.php
@@ -10,6 +10,7 @@
 <?php
 include_once "../template_utils/menuGenerator.php";
 include_once "../utils/sessionUtils.php";
+include_once "../services/printNewsService.php";
 
 loginRequired();
 echo getMenu();
@@ -22,13 +23,59 @@ echo getMenu();
 
 <div class="content">
 
-    <!--TODO: This is the format of the news message that should be printed with backend-->
-    <h2 class="newsHeader"><!--TODO: Here goes header--></h2>
-    <hr>
-    <p class="newsText"><!--TODO: Here goes content of message--></p>
-    <p class="newsDate"><!--TODO: Here goes the date of addition--></p>
+    <?php
+        printNews();
 
-
-
+        echo getPreviousPageLink();
+        echo " <<" . getPage() . ">> ";
+        echo getNextPageLink();
+    ?>
 </body>
 </html>
+
+<?php
+
+function printNews() {
+    $news = printNewsByPage(getPage(), 5);
+
+    if (sizeof($news) === 0) {
+        echo "<h2>No more news</h2>";
+        return;
+    }
+
+    foreach ($news as $item) {
+        echo $item;
+    }
+}
+
+function getPage() {
+
+    if (isset($_GET["page"])) {
+        return $_GET["page"];
+    }
+
+    return 1;
+}
+
+function getPreviousPageLink() {
+    $page = getPage();
+
+    if ($page - 1 !== 0) {
+        return '<a href="../templates/newsPage.php?page=' . ($page - 1) . '">Previous Page</a>';
+    } else {
+        return "";
+    }
+}
+
+
+function getNextPageLink() {
+    $page = getPage();
+
+    if ($page + 1 !== 0) {
+        return '<a href="../templates/newsPage.php?page=' . ($page + 1) . '">Next Page</a>';
+    } else {
+        return "";
+    }
+}
+
+?>
