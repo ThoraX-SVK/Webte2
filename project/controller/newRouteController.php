596e0604e6c3f283ac90202ec1a9a9e4a93212c1
timbez <timbez1996@gmail.com>
2018-05-18 17:23:13 +0200

    news update - changed template to only giving link to next page if there are news available

--- project/controller/newRouteController.php
+++ project/controller/newRouteController.php
@@ -5,10 +5,6 @@ include_once '../utils/sessionUtils.php';
 include_once '../database/routeUtils.php';
 include_once '../database/teamUtils.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 loginRequired();
 
 $distance= getDataFromPOST('distance');
